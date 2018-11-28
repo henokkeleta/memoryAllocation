212f7ac0aa625f1d790424e6e9b09427ab815087
Henok Berhe <henokberhe@henoks-macbook-pro.local>
2018-11-28 11:15:06 -0500

    Initial Commit

---
+++ memoryAllocation/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  memoryAllocation
+//
+//  Created by Henok Berhe on 11/28/18.
+//  Copyright © 2018 Henok Berhe. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
